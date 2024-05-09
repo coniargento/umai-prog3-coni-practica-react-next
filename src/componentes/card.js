500a5f94efe520586b9828a2501a12039e840c87
coniargento <argentoconstanza@gmail.com>
2024-05-09 13:45:26 -0300

    armando componentes card

---
+++ src/componentes/card.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from './Card.module.css'
+
+const Card = (props) => {
+    const { image, title, description } = props
+    return(
+        <div className={styles["contenedorcard"]}>
+            <Image
+            src={image}
+            alt='Next.js Logo'
+            width={200}
+            height={200}
+            priority/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <button>Give me love</button>
+        </div>
+    );
+};
+
+export default Card
